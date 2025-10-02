d19ab79d4059c655b9d5b300cdde857ab8b5a4af
Area15 R&D <muslimsoap@gmail.com>
2025-10-02 13:37:42 +0800

    Create operations.adb

---
+++ operations.adb
@@ -0,0 +1,9 @@
+package body Operations is
+   procedure Transform_Coordinate (X, Y, Z : in out Integer) is
+   begin
+      -- Simple transformation for testing
+      X := X + 1;
+      Y := Y - 1;
+      Z := Z * 2;
+   end Transform_Coordinate;
+end Operations;
