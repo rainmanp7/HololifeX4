fd3f2cdb57a3c304789aa373ee6e87a7fecf261d
Area15 R&D <muslimsoap@gmail.com>
2025-10-02 19:53:53 +0800

    Delete operations.adb

--- operations.adb
+++
@@ -1,8 +0,0 @@
-package body Operations is
-   procedure Transform_Coordinate (X, Y, Z : in out Integer) is
-   begin
-      X := X + 1;
-      Y := Y - 1;
-      Z := Z * 2;
-   end Transform_Coordinate;
-end Operations;
