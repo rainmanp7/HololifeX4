0a3c440a188f831857e08b73e673ed0a1a2bb315
Area15 R&D <muslimsoap@gmail.com>
2025-10-03 20:18:15 +0800

    Update hardware_entity.ads

--- hardware_entity.ads
+++ hardware_entity.ads
@@ -18,9 +18,9 @@ package Hardware_Entity is
    function Check_Flash (Entity : Hardware_Anchor) return Boolean;
    procedure Receive_Pulse (Entity : in out Hardware_Anchor; Sender_ID : Entity_ID);
    
-   -- Hardware validation functions
+   -- Hardware validation functions - TRUTHFUL NAMING
    function Validate_Memory_Layout return Boolean;
-   function Detect_Hardware_Devices return Natural;
+   function Assume_Default_Devices return Natural;  -- HONEST: We assume, not detect
    function Calculate_Resource_Coherence return Natural;
    
    -- Entity constants
