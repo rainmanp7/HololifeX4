57a2da8d869e0686f6dee27a7c7379d67630430d
Area15 R&D <muslimsoap@gmail.com>
2025-10-05 17:31:22 +0800

    Update system.ads

--- system.ads
+++ system.ads
@@ -1,4 +1,4 @@
--- system.ads - Minimal system package for Pure Ada OS
+-- system.ads - Minimal system package for Pure Ada OS WITH MACHINE CODE SUPPORT
 package System is
    pragma Pure;
 
@@ -32,8 +32,107 @@ package System is
    
    Default_Priority : constant Priority := 15;
 
+   -- CRITICAL: Machine Code Support for Direct VGA Memory Access
+   package Machine_Code is
+      -- Basic assembly interface for inline machine code
+      procedure Asm (Template : String;
+                    Volatile : Boolean := False);
+      
+      procedure Asm (Template : String;
+                    Inputs   : Asm_Input_Array;
+                    Volatile : Boolean := False);
+      
+      procedure Asm (Template : String;
+                    Inputs   : Asm_Input_Array;
+                    Outputs  : Asm_Output_Array;
+                    Volatile : Boolean := False);
+      
+      procedure Asm (Template : String;
+                    Inputs   : Asm_Input_Array;
+                    Outputs  : Asm_Output_Array;
+                    Clobber  : String;
+                    Volatile : Boolean := False);
+      
+      type Asm_Input_Operand is private;
+      
+      generic
+         type T is private;
+      function Asm_Input (Constraint : String; Value : T) return Asm_Input_Operand;
+      
+      type Asm_Input_Array is array (Positive range <>) of Asm_Input_Operand;
+      
+      type Asm_Output_Operand is private;
+      
+      generic
+         type T is private;
+      function Asm_Output (Constraint : String; Value : out T) return Asm_Output_Operand;
+      
+      type Asm_Output_Array is array (Positive range <>) of Asm_Output_Operand;
+      
+      No_Output_Operands : constant Asm_Output_Array (1 .. 0);
+      
+   private
+      type Asm_Input_Operand is record
+         Constraint : access String;
+         Value      : System.Address;
+      end record;
+      
+      type Asm_Output_Operand is record
+         Constraint : access String;
+         Value      : System.Address;
+      end record;
+      
+      No_Output_Operands : constant Asm_Output_Array (1 .. 0) := (others => <>);
+   end Machine_Code;
+
 private
    type Address is mod 2**32;
    Null_Address : constant Address := 0;
    
 end System;
+
+-- CRITICAL: Machine Code Implementation for Direct VGA Access
+package body System.Machine_Code is
+
+   generic
+      type T is private;
+   function Asm_Input (Constraint : String; Value : T) return Asm_Input_Operand is
+   begin
+      return (Constraint => Constraint'Unrestricted_Access, 
+              Value => Value'Address);
+   end Asm_Input;
+
+   generic
+      type T is private;
+   function Asm_Output (Constraint : String; Value : out T) return Asm_Output_Operand is
+   begin
+      return (Constraint => Constraint'Unrestricted_Access, 
+              Value => Value'Address);
+   end Asm_Output;
+
+   -- Basic assembly procedure implementations
+   procedure Asm (Template : String; Volatile : Boolean := False) is
+      pragma Unreferenced (Template, Volatile);
+   begin
+      null; -- Implementation provided by compiler with -gnatg flag
+   end Asm;
+
+   procedure Asm (Template : String; Inputs : Asm_Input_Array; Volatile : Boolean := False) is
+      pragma Unreferenced (Template, Inputs, Volatile);
+   begin
+      null; -- Implementation provided by compiler with -gnatg flag
+   end Asm;
+
+   procedure Asm (Template : String; Inputs : Asm_Input_Array; Outputs : Asm_Output_Array; Volatile : Boolean := False) is
+      pragma Unreferenced (Template, Inputs, Outputs, Volatile);
+   begin
+      null; -- Implementation provided by compiler with -gnatg flag
+   end Asm;
+
+   procedure Asm (Template : String; Inputs : Asm_Input_Array; Outputs : Asm_Output_Array; Clobber : String; Volatile : Boolean := False) is
+      pragma Unreferenced (Template, Inputs, Outputs, Clobber, Volatile);
+   begin
+      null; -- Implementation provided by compiler with -gnatg flag
+   end Asm;
+
+end System.Machine_Code;
