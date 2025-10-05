1430ddc66c6f255832450ac0d3fb7de9911dee5f
Area15 R&D <muslimsoap@gmail.com>
2025-10-05 17:52:11 +0800

    Update system.ads

--- system.ads
+++ system.ads
@@ -1,4 +1,4 @@
--- system.ads - Minimal system package for Pure Ada OS WITH MACHINE CODE SUPPORT
+-- system.ads - Minimal system package for Pure Ada OS
 package System is
    pragma Pure;
 
@@ -12,127 +12,20 @@ package System is
    Memory_Size  : constant := 2**32;
 
    type Integer_Address is mod 2**32;
-   
+
    type Bit_Order is (High_Order_First, Low_Order_First);
    Default_Bit_Order : constant Bit_Order := Low_Order_First;
-   
-   Max_Base_Digits       : constant := 15;
-   Max_Digits            : constant := 18;
-   
-   Min_Int               : constant := -(2**(Standard.Integer'Size - 1));
-   Max_Int               : constant := +(2**(Standard.Integer'Size - 1) - 1);
-   
-   Max_Binary_Modulus    : constant := 2**32;
+
+   Max_Base_Digits : constant := 15;
+   Max_Digits : constant := 18;
+
+   Min_Int : constant := -(2**(Standard.Integer'Size - 1));
+   Max_Int : constant := +(2**(Standard.Integer'Size - 1) - 1);
+
+   Max_Binary_Modulus : constant := 2**32;
    Max_Nonbinary_Modulus : constant := 2**31;
-   
+
    -- Priority range
    type Any_Priority is new Integer range 0 .. 31;
    type Priority is new Any_Priority range 0 .. 30;
-   type Interrupt_Priority is new Any_Priority range 31 .. 31;
-   
-   Default_Priority : constant Priority := 15;
-
-   -- CRITICAL: Machine Code Support for Direct VGA Memory Access
-   package Machine_Code is
-      -- Basic assembly interface for inline machine code
-      procedure Asm (Template : String;
-                    Volatile : Boolean := False);
-      
-      procedure Asm (Template : String;
-                    Inputs   : Asm_Input_Array;
-                    Volatile : Boolean := False);
-      
-      procedure Asm (Template : String;
-                    Inputs   : Asm_Input_Array;
-                    Outputs  : Asm_Output_Array;
-                    Volatile : Boolean := False);
-      
-      procedure Asm (Template : String;
-                    Inputs   : Asm_Input_Array;
-                    Outputs  : Asm_Output_Array;
-                    Clobber  : String;
-                    Volatile : Boolean := False);
-      
-      type Asm_Input_Operand is private;
-      
-      generic
-         type T is private;
-      function Asm_Input (Constraint : String; Value : T) return Asm_Input_Operand;
-      
-      type Asm_Input_Array is array (Positive range <>) of Asm_Input_Operand;
-      
-      type Asm_Output_Operand is private;
-      
-      generic
-         type T is private;
-      function Asm_Output (Constraint : String; Value : out T) return Asm_Output_Operand;
-      
-      type Asm_Output_Array is array (Positive range <>) of Asm_Output_Operand;
-      
-      No_Output_Operands : constant Asm_Output_Array (1 .. 0);
-      
-   private
-      type Asm_Input_Operand is record
-         Constraint : access String;
-         Value      : System.Address;
-      end record;
-      
-      type Asm_Output_Operand is record
-         Constraint : access String;
-         Value      : System.Address;
-      end record;
-      
-      No_Output_Operands : constant Asm_Output_Array (1 .. 0) := (others => <>);
-   end Machine_Code;
-
-private
-   type Address is mod 2**32;
-   Null_Address : constant Address := 0;
-   
-end System;
-
--- CRITICAL: Machine Code Implementation for Direct VGA Access
-package body System.Machine_Code is
-
-   generic
-      type T is private;
-   function Asm_Input (Constraint : String; Value : T) return Asm_Input_Operand is
-   begin
-      return (Constraint => Constraint'Unrestricted_Access, 
-              Value => Value'Address);
-   end Asm_Input;
-
-   generic
-      type T is private;
-   function Asm_Output (Constraint : String; Value : out T) return Asm_Output_Operand is
-   begin
-      return (Constraint => Constraint'Unrestricted_Access, 
-              Value => Value'Address);
-   end Asm_Output;
-
-   -- Basic assembly procedure implementations
-   procedure Asm (Template : String; Volatile : Boolean := False) is
-      pragma Unreferenced (Template, Volatile);
-   begin
-      null; -- Implementation provided by compiler with -gnatg flag
-   end Asm;
-
-   procedure Asm (Template : String; Inputs : Asm_Input_Array; Volatile : Boolean := False) is
-      pragma Unreferenced (Template, Inputs, Volatile);
-   begin
-      null; -- Implementation provided by compiler with -gnatg flag
-   end Asm;
-
-   procedure Asm (Template : String; Inputs : Asm_Input_Array; Outputs : Asm_Output_Array; Volatile : Boolean := False) is
-      pragma Unreferenced (Template, Inputs, Outputs, Volatile);
-   begin
-      null; -- Implementation provided by compiler with -gnatg flag
-   end Asm;
-
-   procedure Asm (Template : String; Inputs : Asm_Input_Array; Outputs : Asm_Output_Array; Clobber : String; Volatile : Boolean := False) is
-      pragma Unreferenced (Template, Inputs, Outputs, Clobber, Volatile);
-   begin
-      null; -- Implementation provided by compiler with -gnatg flag
-   end Asm;
-
-end System.Machine_Code;
+   type Interrupt_Priority is
