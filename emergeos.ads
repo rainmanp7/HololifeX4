4ed46bd9d4909906f043025974759bef52b57c14
Area15 R&D <muslimsoap@gmail.com>
2025-10-07 09:36:34 +0800

    Update emergeos.ads

--- emergeos.ads
+++ emergeos.ads
@@ -3,6 +3,7 @@ package EmergeOS is
    pragma Elaborate_Body;  -- Ensure body is elaborated before use
    
    procedure EmergeOS;
-   -- Main operating system procedure
+   pragma Export (C, EmergeOS, "_ada_boot");
+   -- Main operating system procedure exported for C linkage
    
 end EmergeOS;
