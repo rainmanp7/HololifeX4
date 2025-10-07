fede94e55c4232631834d2af2c8f1231bf429451
Area15 R&D <muslimsoap@gmail.com>
2025-10-07 13:34:35 +0800

    Update emergeos.ads

--- emergeos.ads
+++ emergeos.ads
@@ -1,9 +1,5 @@
--- emergeos.ads - HoloXlife OS Package Specification
+-- emergeos.ads - Minimal Boot Test
 package EmergeOS is
-   pragma Elaborate_Body;  -- Ensure body is elaborated before use
-   
-   procedure EmergeOS;
-   pragma Export (C, EmergeOS, "_ada_boot");
-   -- Main operating system procedure exported for C linkage
-   
+   procedure Boot;
+   pragma Export (C, Boot, "_ada_boot");
 end EmergeOS;
